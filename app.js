05ebea4bed86389ddf9c4f007a6679d407a5564f
Yoshinori Hanada <yoshinorihanada@Yoshinoris-MacBook-Pro.local>
2021-02-10 21:45:20 +0900

    added ejs files

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 //jshint esversion:6
 
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const request = require("request");
+
+
 
 const app = express();
 
 app.use(express.static("public"));
+app.set("view engine", "ejs"); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "/signin.html");
-    // res.send("hello world")
+    // res.sendFile(__dirname + "/signin.html");
+    res.render("home");
   
 });
 
+app.get("/signin", function(req,res){
+    res.render("signin");
+});
+
+app.get("/signup", function(req,res){
+    res.render("signup");
+});
 
 
 app.listen(process.env.PORT || 3000, function() {
