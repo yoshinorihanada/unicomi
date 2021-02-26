64a586211aec3a9ee59b15e3b04bcab1604ecf74
Yoshinori Hanada <yoshinorihanada@Yoshinoris-MacBook-Pro.local>
2021-02-26 23:28:12 +0900

    finish profile initialization page except for uploading image

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 
+const fs = require('fs');
+var path = require('path');
 
 
 const app = express();
 
 app.use(express.static("public"));
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
@@ -25,19 +27,21 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/unicomi-user", {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/unicomi-user", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set("useCreateIndex", true);
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    lastName: String,
-    firstName: String,
-    year: Number,
-    month: Number,
-    day: Number,
+    name: String,
+    profileimg:
+    {
+        data: Buffer,
+        contentType: String
+    },
+    grade: String,
     university: String,
-    hobbies: String
+    bio: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -49,80 +53,94 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//set up multer for storing uploaded files
+// var multer = require('multer');
+
+// var storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, '/uploads')
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + '-' + Date.now())
+//     }
+// });
+
+// var upload = multer({ storage: storage });
+
 //setting up the get routes
-app.get("/",function(req,res){
-    
+app.get("/", function (req, res) {
+
     res.render("home");
-  
+
 });
 
-app.get("/signin", function(req,res){
+app.get("/signin", function (req, res) {
     res.render("signin");
 });
 
-app.get("/signup", function(req,res){
+app.get("/signup", function (req, res) {
     res.render("signup");
 });
 
-app.get("/my-profile", function(req,res){
-    
-    if(req.isAuthenticated()){
+app.get("/my-profile", function (req, res) {
+
+    if (req.isAuthenticated()) {
         res.render("my-profile");
-    }else {
+    } else {
         res.redirect("/");
     }
 });
 
-app.get("/logout", function(req,res){
+app.get("/logout", function (req, res) {
     req.logout();
     res.redirect('/');
 });
 
-app.get("/unicomi", function(req,res){
-    if(req.isAuthenticated()){
+app.get("/unicomi", function (req, res) {
+    if (req.isAuthenticated()) {
         res.render("unicomi");
-    }else {
+    } else {
         res.redirect("/signin");
     }
 })
 
-app.get("/createacc", function(req,res){
-    if(req.isAuthenticated()){
+app.get("/createacc", function (req, res) {
+    if (req.isAuthenticated()) {
         res.render("createacc");
-    }else {
+    } else {
         res.redirect("/");
     }
 })
 
-app.get("/filtering", function(req,res){
-    if(req.isAuthenticated()){
+app.get("/filtering", function (req, res) {
+    if (req.isAuthenticated()) {
         res.render("filtering");
-    }else {
+    } else {
         res.redirect("/");
     }
 })
 
-app.get("/edit-profile", function(req,res){
-    if(req.isAuthenticated()){
+app.get("/edit-profile", function (req, res) {
+    if (req.isAuthenticated()) {
         res.render("edit-profile");
-    }else {
+    } else {
         res.redirect("/");
     }
 });
 
 
-app.post("/signin", function(req,res){
+app.post("/signin", function (req, res) {
     const user = new User({
         username: req.body.username,
         password: req.body.password
     });
 
     //passport method
-    req.login(user, function(err){
-        if(err){
+    req.login(user, function (err) {
+        if (err) {
             console.log(err);
-        }else{
-            passport.authenticate("local")(req, res, function(){
+        } else {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/unicomi");
             });
         }
@@ -131,54 +149,64 @@ app.post("/signin", function(req,res){
 });
 
 
-app.post("/signup", function(req,res){
+app.post("/signup", function (req, res) {
 
-    User.register({username: req.body.username}, req.body.password, function(err,user){
-        if(err){
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
             res.redirect("/signup");
-        }else{
-            passport.authenticate("local")(req,res, function(){
+        } else {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/createacc");
             });
         }
     });
 });
 
-app.post("/createacc", function(req,res){
+app.post("/createacc", function (req, res,) {
 
-    const lastName = req.body.lastname;
-    const firstName = req.body.firstname;
-    const year = req.body.year;
-    const month = req.body.month;
-    const day = req.body.day;
+    // upload.single('avator')(req, res, function (error) {
+    //     if (error) {
+    //         console.log(`upload.single error: ${error}`);
+    //         return res.sendStatus(500);
+    //     }
+    //     // code
+
+    // })
+
+    const name = req.body.name;
+    // const profileimg = {
+    //     data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+    //     contentType: 'image/png'
+    // }
+
+    const grade = req.body.grade;
     const university = req.body.university;
-    const hobbies = req.body.hobbies;
+    const bio = req.body.bio;
 
-    User.findById(req.user.id, function(err,foundUser){
-        if(err){
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
             console.log(err);
-        }else {
-            if (foundUser){
-                foundUser.lastname = lastName;
-                foundUser.firstname = firstName;
-                foundUser.year = year;
-                foundUser.day = day;
-                foundUser.month = month;
+        } else {
+            if (foundUser) {
+                foundUser.name = name;
+                // foundUser.profileimg = profileimg;
+                foundUser.grade = grade;
                 foundUser.university = university;
-                foundUser.hobbies = hobbies;
-                foundUser.save(function(){
+                foundUser.bio = bio;
+                foundUser.save(function () {
                     res.redirect("/unicomi");
                 });
             }
         }
     });
-    
+
+
 });
 
 
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port 3000");
 });
